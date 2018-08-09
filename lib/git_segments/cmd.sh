22450777395bff7f02f31fb848a1bc660d6bf3df
jtzero <jtzero511@gmail.com>
2018-08-09 15:04:59 -0600

    add fix for cmd format

--- lib/git_segments/cmd.sh
+++ lib/git_segments/cmd.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-echo "$(git rev-parse --short HEAD 2> /dev/null)$(git status -b --porcelain 2> /dev/null)"
+echo "#%$(git rev-parse --short HEAD 2> /dev/null)
+$(git status -b --porcelain 2> /dev/null)"
